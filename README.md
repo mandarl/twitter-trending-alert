251440844bc7e33fa62b575182f227c1caf7cd3a
mandarlimaye123 <mandarlimaye@yahoo.com>
2012-11-20 11:03:10 +0530

    readme

--- README.md
+++ README.md
@@ -8,6 +8,18 @@ Python script that monitors twitter trending topics and alert if a new topic is 
 * Configurable polling interval
 * Configurable location (currently worldwide)
 
+Installation
+------------
+
+To install clint, simply: ::
+
+    $ pip install clint
+
+Or, if you absolutely must: ::
+
+    $ easy_install clint
+
+But, you really shouldn't do that.
 
 **SCREENSHOT:**
 
--- twitter-trending-alert.py
+++ twitter-trending-alert.py
@@ -1,15 +1,20 @@
+import sys
 import os
 from time import sleep
 from random import random
 from clint.textui import progress, puts, indent, colored
 import twitter
 
-TIME_INTERVAL_IN_SECONDS = 30 
+TIME_INTERVAL_IN_SECONDS = 30
+#WOEIDS at http://engineering.twitter.com/2010/02/woeids-in-twitters-trends.html
+WORLD_WOE_ID = 1
+US_WOE_ID = 23424977
+WOE_ID = US_WOE_ID
+ 
 
 def getTrends():
     twitter_api=twitter.Twitter(domain="api.twitter.com", api_version='1')
-    WORLD_WOE_ID = 1
-    world_trends = twitter_api.trends._(WORLD_WOE_ID)
+    world_trends = twitter_api.trends._(WOE_ID)
     trends = [ trend for trend in world_trends()[0]['trends'] ]
     return trends
 
@@ -24,26 +29,29 @@ def isTrendNew(trend_name, old_trends):
 def main():
     old_trends = {}
     while 1==1:
-        print "\nTrending topis:\n"
-    
-        new_trends = getTrends()
-        for trend in new_trends:
-            trend_name = trend['name']
-            if isTrendNew(trend_name, old_trends):
-            #if trend_name == 'Tyga':
-                with indent(8, quote=colored.red(' |')):
-                    puts(colored.red('  NEW'))
-		    print trend['name']
-            else:
-                with indent(8, quote=colored.cyan(' |')):
-                    puts(".")
-		    print trend['name']
-        old_trends = new_trends
+        isNew = False
+        try:
+            print "\nTrending topics:\n"
         
-        print "\n\nRefreshing in:"
-        for i in progress.bar(range(TIME_INTERVAL_IN_SECONDS)):
-            sleep(1)
-        os.system( [ 'clear', 'cls' ][ os.name == 'nt' ] )
+            new_trends = getTrends()
+            for trend in new_trends:
+                trend_name = trend['name']
+                if isTrendNew(trend_name, old_trends):
+                #if trend_name == 'Tyga':
+                    isNew = True
+                    sys.stdout.write("\t" + trend['name'])
+                    puts(colored.red('  NEW'))
+                else:
+                    print "\t" + trend['name']
+            if isNew:
+                print "\a\a\a"
+            old_trends = new_trends
+            
+            print "\n\nRefreshing in:"
+            for i in progress.bar(range(TIME_INTERVAL_IN_SECONDS)):
+                sleep(1)
+        finally:
+            os.system( [ 'clear', 'cls' ][ os.name == 'nt' ] )
 
 
 main()
